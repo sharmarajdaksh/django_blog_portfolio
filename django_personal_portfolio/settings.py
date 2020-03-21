92a40b6d162913024c91cd581cb11b8bb453ec87
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2020-03-21 09:04:23 +0530

    Update settings.py

--- django_personal_portfolio/settings.py
+++ django_personal_portfolio/settings.py
@@ -117,10 +117,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/admin/login/?next=/admin/
 
 STATIC_URL = '/static/'
-STATIC _ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
